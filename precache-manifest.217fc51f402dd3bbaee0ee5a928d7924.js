be37ebd9ed1d20b4475ac19c8e748c5ee980431a
Kenji Katahira <kkatahira@uolinc.com>
2020-12-01 09:25:47 -0300

    Updates

---
+++ precache-manifest.217fc51f402dd3bbaee0ee5a928d7924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d9a01e4e739a200c86cd0584bd3ed7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99e603081d0132719cb5",
+    "url": "./static/css/main.f185306f.chunk.css"
+  },
+  {
+    "revision": "95c2e1f04080c6b6c80e",
+    "url": "./static/js/2.754d7af4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.754d7af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e603081d0132719cb5",
+    "url": "./static/js/main.9f308e37.chunk.js"
+  },
+  {
+    "revision": "e9871e1610e842c0a455",
+    "url": "./static/js/runtime-main.6e6c0416.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./react-spotify-player/precache-manifest.85592ca74cf6a6a21d8c61aeecb1b2cb.js"
+  "./precache-manifest.217fc51f402dd3bbaee0ee5a928d7924.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./react-spotify-player/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
