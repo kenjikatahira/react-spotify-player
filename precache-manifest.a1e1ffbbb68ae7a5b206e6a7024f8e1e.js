555fb3bd1ed88d87855409c01299ef3c729c9a1b
Kenji Katahira <kkatahira@uolinc.com>
2020-12-01 09:19:54 -0300

    Updates

---
+++ precache-manifest.a1e1ffbbb68ae7a5b206e6a7024f8e1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326c7e85c421c3474388089fd9a31695",
+    "url": "./build/index.html"
+  },
+  {
+    "revision": "c161fd93b05d05cf023e",
+    "url": "./build/static/css/main.f185306f.chunk.css"
+  },
+  {
+    "revision": "2b6d0236a537c24f2940",
+    "url": "./build/static/js/2.4b020584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./build/static/js/2.4b020584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c161fd93b05d05cf023e",
+    "url": "./build/static/js/main.3b13f0d4.chunk.js"
+  },
+  {
+    "revision": "924219520311229bb350",
+    "url": "./build/static/js/runtime-main.8dd7b3fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./build/static/media/logo.5d5d9eef.svg"
+  }
+]);
