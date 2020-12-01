e4c35bf6412102fcd8fb8a976318ba2ef7d33deb
Kenji Katahira <kkatahira@uolinc.com>
2020-12-01 10:02:57 -0300

    Updates

---
+++ precache-manifest.e25d029ee576370cd505e4f3c3bd0201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f69454a3ddef089dd639fd18e142a8c",
+    "url": "./react-spotify-player/index.html"
+  },
+  {
+    "revision": "eb2d4336d44f846479db",
+    "url": "./react-spotify-player/static/css/main.8c5898bb.chunk.css"
+  },
+  {
+    "revision": "3a6733fcbae24937d53c",
+    "url": "./react-spotify-player/static/js/2.c7b46d0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./react-spotify-player/static/js/2.c7b46d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2d4336d44f846479db",
+    "url": "./react-spotify-player/static/js/main.b8b19d0b.chunk.js"
+  },
+  {
+    "revision": "a3a043f74132f425e223",
+    "url": "./react-spotify-player/static/js/runtime-main.ace833ef.js"
+  }
+]);
