e776e368c3ba236138d9f45bb68d1856802fd118
Kenji Katahira <kkatahira@uolinc.com>
2020-12-10 01:42:58 -0300

    ✍ - refactoring app

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const clientId = '';
-export const clientSecret  = '';
-export const redirectUri = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://kenjikatahira.github.io/react-spotify-player/';
